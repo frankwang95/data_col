2cfe317e3ca44d04462b95ef170c59a7d35003f1
frankwang <fkwang@uchicago.edu>
2016-07-17 01:18:33 -0500

    Added support for remote connections in client library

--- client.py
+++ client.py
@@ -8,7 +8,7 @@ from utils import snd, recv
 
 # mechS - See mechanisms.py
 # ret - 'default', 'pathtowrite'
-def putget(data, port, name = 'default', mechS = 'paused', ret = 'default'):
+def putget(data, port, ip = 'localhost', name = 'default', mechS = 'paused', ret = 'default'):
 	if mechS not in mech.indexS:
 		print('mechS argument invalid, putget failed')
 		return(1)
@@ -18,7 +18,7 @@ def putget(data, port, name = 'default', mechS = 'paused', ret = 'default'):
 			print('ret argument invalid, putget failed')
 			return(1)
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.connect(('localhost', port))
+	s.connect((ip, port))
 	string = ' '.join([name] + [mechS] + [ret] + [str(i) for i in data])
 	retOut = 0
 	snd(s, string)
